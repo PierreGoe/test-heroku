55a628755ef8ec680a898129b386990f4093390a
Pierre Goemans <pierre.goemans.ext@melting-point.fr>
2022-09-19 14:28:29 +0200

    test Puppeter

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
+const puppeteer = require('puppeteer');
 
-
-app.listen((process.env.PORT || 5000), () => {
+let page
+app.listen((process.env.PORT || 5000), async () => {
     console.log('API now available on http://localhost:5050 !');
+
+
 })
 
 
 app.get('/', async (req, res) => {
-    res.status(200).send('Coucou !');
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
+    page = await browser.newPage();
+    await page.goto('https://rbixm.qualtrics.com/jfe/form/SV_9MHgHFvPm0OEHr0?CountryCode=FRA&Q_Language=FR&PT=1');
+    let htmlSoup = await page.$eval('.QuestionText', el => el.innerHTML)
+
+    res.status(200).send(htmlSoup);
 
 });
